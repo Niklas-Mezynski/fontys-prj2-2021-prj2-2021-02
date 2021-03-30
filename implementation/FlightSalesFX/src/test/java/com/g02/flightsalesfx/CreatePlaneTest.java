da61a82590ce874c117baad4cfcb3595b12b11d9
SchusterEric <e.schuster@student.fontys.nl>
2021-03-30 18:47:46 +0200

    Increase coverage

--- implementation/FlightSalesFX/src/test/java/com/g02/flightsalesfx/CreatePlaneTest.java
+++ implementation/FlightSalesFX/src/test/java/com/g02/flightsalesfx/CreatePlaneTest.java
@@ -1,5 +1,6 @@
 package com.g02.flightsalesfx;
 
+import com.g02.flightsalesfx.businessEntities.Seat;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,7 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -36,10 +38,38 @@ public class CreatePlaneTest {
     void createPlaneOpens(FxRobot nils) {
         //See the comment above if this produces a weird error
         assertThat(stage.getTitle()).isEqualTo("Flight Ticket Sales");
-        Parent rootOfElements = stage.getScene().getRoot();
+        Assertions.assertThat(nils.lookup(".label").queryAs(Label.class)).doesNotHaveText("Create Plane");
         Assertions.assertThat(nils.lookup("#goToCreatePlane").queryAs(Button.class)).hasText("Create Plane");
         nils.clickOn(nils.lookup("#goToCreatePlane").queryAs(Button.class));
+        //Now in createPlane
         Assertions.assertThat(nils.lookup(".label").queryAs(Label.class)).hasText("Create Plane");
+    }
+
+    @Test
+    void addRowAndSeatWorks(FxRobot nils){
+        nils.clickOn(nils.lookup("#goToCreatePlane").queryAs(Button.class));
+        //Now in createPlane
+        Assertions.assertThat(nils.lookup(".label").queryAs(Label.class)).hasText("Create Plane");
+        nils.clickOn(nils.lookup("#addRow").queryAs(Button.class));
+        var buttons=nils.lookup((s)-> s instanceof Button).queryAllAs(Button.class);
+        var a=false;
+        Button addButton=null;
+        for (Button button : buttons) {
+            if (button.getText().equals("ADD")){
+                a = true;
+                addButton=button;
+            }
+        }
+        assertThat(a).isTrue();
+        nils.clickOn(addButton);
+        buttons=nils.lookup((s)-> s instanceof Button).queryAllAs(Button.class);
+        a=false;
+        for (Button button : buttons) {
+            if (button.getText().equals("01A")){
+                a = true;
+            }
+        }
+        assertThat(a).isTrue();
     }
 
 }
