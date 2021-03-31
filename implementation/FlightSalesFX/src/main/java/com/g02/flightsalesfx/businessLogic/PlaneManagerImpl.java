6273b5a37b0cfbebec128cc9a4408a907556fb2d
PhilipKuntges <p.kuntges@student.fontys.nl>
2021-03-31 11:03:03 +0200

    Update PlaneManagerImpl.java

--- implementation/FlightSalesFX/src/main/java/com/g02/flightsalesfx/businessLogic/PlaneManagerImpl.java
+++ implementation/FlightSalesFX/src/main/java/com/g02/flightsalesfx/businessLogic/PlaneManagerImpl.java
@@ -1,7 +1,10 @@
 package com.g02.flightsalesfx.businessLogic;
 
+import java.util.List;
+
 import com.g02.flightsalesfx.businessEntities.Plane;
 import com.g02.flightsalesfx.businessEntities.PlaneManager;
+import com.g02.flightsalesfx.businessEntities.Seat;
 import com.g02.flightsalesfx.persistence.PlaneStorageService;
 
 public class PlaneManagerImpl implements PlaneManager {
@@ -11,6 +14,13 @@ public class PlaneManagerImpl implements PlaneManager {
     @Override
     public Plane createPlane(String name, String manufacturer, String type) {
         return new PlaneImpl(name, manufacturer, type);
+    }
+
+    public Plane createPlane(String name, String manufacturer, String type, List<? extends Seat> seatList) {
+        Plane newPlane = new PlaneImpl(name, manufacturer, type);
+        newPlane.addAllSeats(seatList);
+        
+        return newPlane;
     }
 
     public void setPlaneStorageService(PlaneStorageService planeStorageService) {
