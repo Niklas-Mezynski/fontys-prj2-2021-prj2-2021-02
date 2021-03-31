08613fb54389b6d8061d7dd665c2448606902751
Nils Winking <n.winking@student.fontys.nl>
2021-03-31 11:47:00 +0200

    test add arguments

--- implementation/FlightSalesFX/src/main/java/module-info.java
+++ implementation/FlightSalesFX/src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.g02.flightsalesfx {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens com.g02.flightsalesfx;
+    opens com.g02.flightsalesfx to javafx.fxml;
     opens com.g02.flightsalesfx.businessEntities;
     opens com.g02.flightsalesfx.businessLogic;
     opens com.g02.flightsalesfx.persistence;
