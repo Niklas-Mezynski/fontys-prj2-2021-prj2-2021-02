734d238e869d33f6bfb19aa8a82b4b7810c10645
Nils Winking <n.winking@student.fontys.nl>
2021-03-31 10:52:44 +0200

    export all packages to fxml

--- implementation/FlightSalesFX/src/main/java/module-info.java
+++ implementation/FlightSalesFX/src/main/java/module-info.java
@@ -3,5 +3,8 @@ module com.g02.flightsalesfx {
     requires javafx.fxml;
 
     opens com.g02.flightsalesfx;
+    opens com.g02.flightsalesfx.businessLogic;
+    opens com.g02.flightsalesfx.businessEntities;
+    opens com.g02.flightsalesfx.persistence;
     exports com.g02.flightsalesfx;
 }
