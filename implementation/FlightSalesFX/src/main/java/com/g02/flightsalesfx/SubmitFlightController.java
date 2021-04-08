fcb25dfdfdf813ba71816852917dca84b18d252d
Niklas-Mezynski <70883487+Niklas-Mezynski@users.noreply.github.com>
2021-04-08 13:19:51 +0200

    .

---
+++ implementation/FlightSalesFX/src/main/java/com/g02/flightsalesfx/SubmitFlightController.java
@@ -0,0 +1,36 @@
+package com.g02.flightsalesfx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class SubmitFlightController {
+
+    @FXML
+    private TextField planeSearchBar;
+
+    @FXML
+    private AnchorPane planeTablePane;
+
+    @FXML
+    private TextField flightPrice;
+
+    @FXML
+    private Button saveFlightButton;
+
+    @FXML
+    private Button exitFlightButton;
+
+    @FXML
+    void exit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveFlight(ActionEvent event) {
+
+    }
+
+}
