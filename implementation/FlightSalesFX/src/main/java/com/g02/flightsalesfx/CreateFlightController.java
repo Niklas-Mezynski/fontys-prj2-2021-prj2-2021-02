a7d965059b156f4032090d8896efab49984f6f8f
Luca-Engels <Luca.Engels@student.fontys.nl>
2021-04-08 09:54:45 +0200

    created flight controller
    Co-Authored-By: Philip Küntges <70883117+PhilipKuntges@users.noreply.github.com>
    Co-Authored-By: Niklas-Mezynski <70883487+Niklas-Mezynski@users.noreply.github.com>
    Co-Authored-By: anatol-pavl <70883852+anatol-pavl@users.noreply.github.com>

---
+++ implementation/FlightSalesFX/src/main/java/com/g02/flightsalesfx/CreateFlightController.java
@@ -0,0 +1,36 @@
+package com.g02.flightsalesfx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CreateFlightController {
+
+    @FXML
+    private TextField routeSearchBar;
+
+    @FXML
+    private Button createRouteButton;
+
+    @FXML
+    private Button saveFlightButton;
+
+    @FXML
+    private Button exitFlightButton;
+
+    @FXML
+    void createRoute() {
+
+    }
+
+    @FXML
+    void exit() {
+
+    }
+
+    @FXML
+    void saveFlight() {
+
+    }
+
+}
