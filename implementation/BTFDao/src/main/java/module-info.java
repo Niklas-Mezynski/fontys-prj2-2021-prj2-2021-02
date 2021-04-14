50d18e1ab2048be16118bf6af889714f532ce524
Nils Winking <n.winking@student.fontys.nl>
2021-04-14 14:26:28 +0200

    Final Final Version 3 fix v2 fix (final) final fix fix

--- implementation/BTFDao/src/main/java/module-info.java
+++ implementation/BTFDao/src/main/java/module-info.java
@@ -1,9 +1,10 @@
 module BTFDao {
     requires java.sql;
-    requires org.postgresql.jdbc;
     requires java.naming;
+    requires org.postgresql.jdbc;
     exports com.g02.btfdao.dao;
     exports com.g02.btfdao.annotations;
     exports com.g02.btfdao.queries;
     exports com.g02.btfdao.utils;
+    opens com.g02.btfdao.utils;
 }
