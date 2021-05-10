8ac5a1e4b690a7fa3a092117d6bacd615d1d5e3d
Niklas-Mezynski <n.mezynski@student.fontys.nl>
2021-05-10 10:28:22 +0200

    Added SampleData for DBCreation

--- implementation/FlightSalesFX/src/test/java/org/g02/flightsalesfx/helpers/CreateDB.java
+++ implementation/FlightSalesFX/src/test/java/org/g02/flightsalesfx/helpers/CreateDB.java
@@ -3,6 +3,8 @@ package org.g02.flightsalesfx.helpers;
 import org.g02.btfdao.dao.Dao;
 import org.g02.btfdao.dao.PGJDBCUtils;
 import org.g02.btfdao.dao.Savable;
+import org.g02.flightsalesfx.businessEntities.Airport;
+import org.g02.flightsalesfx.businessEntities.PlaneManager;
 import org.g02.flightsalesfx.persistence.PersistenceAPIImpl;
 import org.g02.flightsalesfx.businessLogic.*;
 import org.junit.jupiter.api.Disabled;
@@ -11,6 +13,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Disabled
@@ -57,7 +61,7 @@ public class CreateDB {
     }
 
     @Test
-    void t03insertAirports() {
+    void t03insertAirportsAndRoutes() {
         var persistenceAPI = new PersistenceAPIImpl();
         var airportStorageService = persistenceAPI.getAirportStorageService(new AirportManagerImpl());
         System.out.println(airportStorageService.add(new AirportImpl("BRE","Bremen","Germany")));
@@ -68,5 +72,40 @@ public class CreateDB {
         System.out.println(airportStorageService.add(new AirportImpl("DUS", "Düsseldorf", "Germany")));
         System.out.println(airportStorageService.add(new AirportImpl("BER", "Berlin", "Germany")));
         System.out.println(airportStorageService.add(new AirportImpl("FRA", "Frankfurt", "Germany")));
+
+        var routeStorageService = persistenceAPI.getRouteStorageService(new RouteManagerImpl());
+        List<Airport> all = airportStorageService.getAll();
+        System.out.println(routeStorageService.add(new RouteImpl(all.get(0), all.get(1))));
+        System.out.println(routeStorageService.add(new RouteImpl(all.get(1), all.get(2))));
+        System.out.println(routeStorageService.add(new RouteImpl(all.get(2), all.get(3))));
+
+    }
+
+    @Test
+    void t04insertPlanes() {
+        var persistenceAPI = new PersistenceAPIImpl();
+        var planeStorageService = persistenceAPI.getPlaneStorageService(new PlaneManagerImpl());
+        PlaneImpl plane = new PlaneImpl("Flugzeug", "B418", "Airbus");
+        List<SeatImpl> seats = new ArrayList<>();
+        seats.add(new SeatImpl(0, 0));
+        seats.add(new SeatImpl(0, 1));
+        seats.add(new SeatImpl(1, 0));
+        seats.add(new SeatImpl(1, 1));
+        plane.addAllSeats(seats);
+        System.out.println(planeStorageService.add(plane));
+    }
+
+    @Test
+    void t05insertFlight() {
+        var persistenceAPI = new PersistenceAPIImpl();
+        var flightStorageService = persistenceAPI.getFlightStorageService(new FlightManagerImpl());
+        System.out.println(flightStorageService.add(new FlightImpl(
+                (SalesOfficerImpl) persistenceAPI.getEmployeeStorageService(new EmployeeManagerImpl()).get("o").get(),
+                LocalDateTime.of(2021, 6, 5, 12, 30),
+                LocalDateTime.of(2021, 6, 5, 14, 30),
+                persistenceAPI.getRouteStorageService(new RouteManagerImpl()).getAll().get(0),
+                persistenceAPI.getPlaneStorageService(new PlaneManagerImpl()).getAll().get(0),
+                85.99
+        )));
     }
 }
