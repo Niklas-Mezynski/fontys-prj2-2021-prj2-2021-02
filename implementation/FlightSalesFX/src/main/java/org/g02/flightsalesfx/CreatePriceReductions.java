20f8d77f400d3b85d4e1b04daa8b5dc611db9f33
Luca-Engels <Luca.engels@student.fontys.nl>
2021-05-20 14:02:15 +0200

    Bug Fixes

--- implementation/FlightSalesFX/src/main/java/org/g02/flightsalesfx/CreatePriceReductions.java
+++ implementation/FlightSalesFX/src/main/java/org/g02/flightsalesfx/CreatePriceReductions.java
@@ -142,8 +142,10 @@ public class CreatePriceReductions implements Controller {
         flightTable.setOnMouseClicked(e->{
             flight=flightTable.getSelectionModel().getSelectedItem();
             if(reduction!=null){
-                saveButton.setDisable(false);
-                saveButton.setText("Add \""+reduction.getName()+"\" to Flight Nr. \""+flight.getFlightNumber()+"\"");
+                if(flight!=null) {
+                    saveButton.setDisable(false);
+                    saveButton.setText("Add \"" + reduction.getName() + "\" to Flight Nr. \"" + flight.getFlightNumber() + "\"");
+                }
             }
         });
         flightTable.setMinWidth(400);
@@ -158,8 +160,10 @@ public class CreatePriceReductions implements Controller {
         reductionTable.setOnMouseClicked(e->{
             reduction=reductionTable.getSelectionModel().getSelectedItem();
             if(flight!=null){
-                saveButton.setDisable(false);
-                saveButton.setText("Add \""+reduction.getName()+"\" to Flight Nr. \""+flight.getFlightNumber()+"\"");
+                if(reduction!=null) {
+                    saveButton.setDisable(false);
+                    saveButton.setText("Add \"" + reduction.getName() + "\" to Flight Nr. \"" + flight.getFlightNumber() + "\"");
+                }
             }
         });
     }
--- implementation/FlightSalesFX/src/main/java/org/g02/flightsalesfx/gui/PriceReductionsTable.java
+++ implementation/FlightSalesFX/src/main/java/org/g02/flightsalesfx/gui/PriceReductionsTable.java
@@ -1,5 +1,6 @@
 package org.g02.flightsalesfx.gui;
 
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableColumn;
@@ -8,24 +9,35 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import org.g02.flightsalesfx.businessEntities.PriceReduction;
 
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+
 
 public class PriceReductionsTable extends TableView<PriceReduction> {
     public PriceReductionsTable(List<PriceReduction> prices, RowClickHandler<PriceReduction> clickHandler) {
         TableColumn<PriceReduction,String > nameColumn = new TableColumn<>("Name");
         nameColumn.setCellValueFactory(v->{return new SimpleStringProperty(v.getValue().getName());});
         TableColumn<PriceReduction,String > redColumn = new TableColumn<>("Reduction");
-        redColumn.setCellValueFactory(v->{return new SimpleStringProperty(v.getValue().getPercentageAsDouble()+"");});
+        redColumn.setCellValueFactory(v->{
+            var perc= new SimpleStringProperty(v.getValue().getPercentageAsDouble()+"");
+            var isPerc=new SimpleBooleanProperty(v.getValue().isPercentage());
+            if(isPerc.get()){
+                perc.setValue(perc.getValue()+"%");
+            }else{
+                perc.setValue(perc.getValue()+"€");
+            }
+            return perc;
+        });
         TableColumn<PriceReduction,String > startColumn = new TableColumn<>("Start Date");
-        startColumn.setCellValueFactory(v->{return new SimpleStringProperty(v.getValue().getStartTime()+"");});
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
+        startColumn.setCellValueFactory(v->{return new SimpleStringProperty(v.getValue().getStartTime().format(myFormatObj).substring(0,10));});
         TableColumn<PriceReduction,String > endColumn = new TableColumn<>("End Date");
-        endColumn.setCellValueFactory(v->{return new SimpleStringProperty(v.getValue().getEndTime()+"");});
-        TableColumn<PriceReduction,String > percentColumn = new TableColumn<>("%");
-        percentColumn.setCellValueFactory(v->{return new SimpleStringProperty(v.getValue().isPercentage()+"");});
+        endColumn.setCellValueFactory(v->{return new SimpleStringProperty(v.getValue().getEndTime().format(myFormatObj).substring(0,10));});
         setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
         getItems().addAll(prices);
         getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-        getColumns().addAll(nameColumn, redColumn, startColumn, endColumn, percentColumn);
+        getColumns().addAll(nameColumn, redColumn, startColumn, endColumn);
         setRowFactory(planeTableView -> {
             TableRow<PriceReduction> row = new TableRow<>();
             row.setOnMouseClicked(mouseEvent -> {
