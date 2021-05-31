bb3202801439f642fcc1611e44cddcd3d9ec1b5e
Nils Winking <n.winking@student.fontys.nl>
2021-05-31 09:57:49 +0200

    tests to verify adding seats works and create plane works

--- implementation/FlightSalesFX/src/test/java/org/g02/flightsalesfx/businessLogic/PlaneTest.java
+++ implementation/FlightSalesFX/src/test/java/org/g02/flightsalesfx/businessLogic/PlaneTest.java
@@ -23,6 +23,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -44,12 +45,14 @@ public class PlaneTest {
             new SeatImpl(1, 0, List.of(seatOptionBClass)),
             new SeatImpl(1, 1, List.of(seatOptionBClass, seatOptionBClass))
     );
-    private final Plane plane2 = new PlaneImpl(2, "D-BCDE", "B", "B").addAllSeats(seats);
+    private final Plane plane2 = new PlaneImpl(1, "D-BCDE", "B", "B").addAllSeats(seats);
+    private final Plane plane3 = new PlaneImpl(1, "D-CDEF", "C", "B");
+    private final Plane plane4 = new PlaneImpl(1, "D-ABCD", "A", "A").addAllSeats(seats);
     private List<SeatOptionImpl> allSeatOptions = List.of(seatOptionFirstClass, seatOptionBClass, seatOptionLegRoom);
     private List<Plane> planes = List.of(plane1);
 
-    private Map<String, Plane> planeHashMap = Map.of("1", plane1, "2", plane2);
-    private Map<String, List<Seat>> seatMap = Map.of("1", seats);
+    private Map<String, Plane> planeHashMap = Map.of("1", plane1, "2", plane2, "3", plane3, "4", plane4);
+    private Map<String, List<Seat>> seatMap = Map.of("0", List.of(), "1", seats);
 
     @Start
     void start(Stage stage) throws IOException {
@@ -71,9 +74,21 @@ public class PlaneTest {
         TestUtil.verifyEqualsHasCode(ref, eql, uneql1, uneql2, uneql3);
     }
 
+    /**
+     * Test that the business logic updates the plane correctly and the correct object is passed to the persistence layer
+     * @param keyOld
+     * @param keyNew
+     * @param newName
+     * @param newMan
+     * @param newType
+     * @param seatKey
+     */
     @ParameterizedTest
     @CsvSource({
             "1,2,D-BCDE,B,B,1",
+            "1,1,D-ABCD,A,A,0",
+            "1,3,D-CDEF,C,B,0",
+
     })
     void t01testUpdateProducesCorrectPlanes(String keyOld, String keyNew, String newName, String newMan, String newType, String seatKey) {
         var planeOld = planeHashMap.get(keyOld);
@@ -83,7 +98,9 @@ public class PlaneTest {
         ArgumentCaptor<Plane> planeArgumentCaptor = ArgumentCaptor.forClass(Plane.class);
         verify(persistenceAPI, times(2)).getPlaneStorageService(any());
         verify(planeStorageService).update(planeArgumentCaptor.capture());
-        Assertions.assertThat(planeArgumentCaptor.getValue()).isEqualTo(planeNew);
+        assertThat(planeArgumentCaptor.getValue()).isEqualTo(planeNew);
+        assertThat(planeArgumentCaptor.getValue().getId()).isEqualTo(planeOld.getId());
+        assertThat(planeArgumentCaptor.getValue().getId()).isEqualTo(planeNew.getId());
     }
 
     @ParameterizedTest
@@ -94,5 +111,36 @@ public class PlaneTest {
         PlaneImpl plane = (PlaneImpl) planeHashMap.get(key);
         businessLogicAPI.deletePlane(plane);
         verify(planeStorageService).delete(any());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1,4,1,true",
+            "1,4,1,false"
+    })
+    void t03addSeatWorking(String plane, String expectedPlane, String seats, boolean individual) {
+        PlaneImpl planeOld = (PlaneImpl) planeHashMap.get(plane);
+        PlaneImpl planeExpected = (PlaneImpl) planeHashMap.get(expectedPlane);
+        var seatsList = seatMap.get(seats);
+        if (individual) {
+            seatsList.forEach(planeOld::addSeat);
+        } else {
+            planeOld.addAllSeats(seatsList);
+        }
+        assertThat(planeOld).isEqualTo(planeExpected);
+        assertThat(planeOld.getAllSeats()).containsAll(seatsList);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "D-ABCD,A,A,0,1",
+            "D-BCDE,B,B,1,2",
+    })
+    void t04testCreatePlaneMethodWorking(String name, String man, String type, String seatsKey, String planeKey) {
+        businessLogicAPI.createPlaneFromUI(name, man, type, seatMap.get(seatsKey));
+        var plane = planeHashMap.get(planeKey);
+        ArgumentCaptor<Plane> planeArgumentCaptor = ArgumentCaptor.forClass(Plane.class);
+        verify(planeStorageService).add(planeArgumentCaptor.capture());
+        assertThat(planeArgumentCaptor.getValue()).isEqualTo(plane);
     }
 }
